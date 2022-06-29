328263cf68764e28b213b5be4db5f6247d30a3cd
Everson de Olveira <82684218+Eor13@users.noreply.github.com>
2022-06-28 22:16:02 -0300

    Alterações

--- aula12ex/ex13/script.js
+++ aula12ex/ex13/script.js
@@ -7,33 +7,33 @@ function verif(){
     if (fano.value.length == 0 || Number(fano.value) > ano){
         window.alert('[ERRO] Verifique os dados e tente novamente!')
     } else {
-        var fsex = window.document.getElementsByName('radsex')
+        var fsex = window.document.getElementsByName('radsex')//name="readsex" linha 22
         var idade = ano -Number(fano.value)
         var genero = ''
         var img = window.document.createElement('img')
-        img.setAttribute('id','foto')
+        img.setAttribute('id','foto')//Inserção  de imagem
         if (fsex[0].checked){
             genero ='Homem'
             if(idade>=0 && idade<=12){
                 img.setAttribute('src', 'image/menino.png')
-        }else if( idade < 21){
+            }else if( idade < 21){
             img.setAttribute('src', 'image/adolescentem.png')
-        }else if( idade <60){
+            }else if( idade <60){
             img.setAttribute('src', 'image/homem.png')
-        }else{
+            }else{
             img.setAttribute('src', 'image/idoso.png')
-        }
+            }
         }else if (fsex[1].checked){
             genero = 'Mulher'
             if(idade>=0 && idade<=12){
                 img.setAttribute('src', 'image/menina.png')
-        }else if( idade <= 21){
+            }else if( idade <= 21){
             img.setAttribute('src', 'image/adolescentef.png')
-        }else if( idade <60){
+            }else if( idade <60){
             img.setAttribute('src', 'image/mulher.png')
-        }else{
+            }else{
             img.setAttribute('src', 'image/idosa.png')
-        }
+            }
         }   
         res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
         res.style.textAlign ='center'
