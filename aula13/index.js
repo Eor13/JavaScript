a9dfe1de97d05d620db64e042fb78595e9dec132
Everson de Olveira <82684218+Eor13@users.noreply.github.com>
2022-06-28 22:40:00 -0300

    Create index.js

---
+++ aula13/index.js
@@ -0,0 +1,25 @@
+var c =1
+/* Teste lógico no início
+do{
+    console.log('Tudo bem?')
+    c++
+}while(c<=6)
+*/
+
+//Teste lógico no final
+while(c<=10){
+    console.log('Tudo bem?')
+    c++
+}
+
+
+
+
+
+/*console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
