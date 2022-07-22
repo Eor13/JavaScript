9989530dcb6c9bf7f6cac972793a06ead880f4cc
Everson de Olveira <82684218+Eor13@users.noreply.github.com>
2022-07-22 19:44:11 -0300

    Exercicio

---
+++ aula14/alet.js
@@ -0,0 +1,5 @@
+if (star.value.length==0 || encodeURI.value.length ==0 ||jump.value.length ==00){
+    window.alert('[Error] Faltam dados')
+}else{
+    res.innerHTML = 'tudo ok!'
+}
---
+++ aula14ex/ex15/script.js
@@ -0,0 +1,43 @@
+
+function contar(){
+var star = window.document.getElementById('inicio')
+var end = window.document.getElementById('fim')
+var passo = window.document.getElementById('intervalo')
+var res = window.document.getElementById('res')
+
+if (star.value.length == 0 || end.value.length == 0 || passo.value.length == 00) {
+    res.innerHTML += ` Impossível contar.`
+    //window.alert('[Error] Faltam dados')
+}else{
+    res.innerHTML = 'Contando <br>'
+    var i = Number(star.value)
+    var f = Number(end.value)
+    var p = Number(passo.value)
+    var c = i
+    if (p <= 0){
+        window.alert('Passo inválido! Considerando o PASSO 1')
+        p = 1
+    }
+
+    if(i<f){
+        //Contagem crescente
+    do{
+        res.innerHTML+= `${c}\u{1F449}`
+        c+= p
+    }while(c<=f)
+    }else{
+        //Contagem Descrescente
+        do{
+            res.innerHTML +=`${c} \u{1f449}`
+            c-=p
+        } while(c>=f)
+    }
+    res.innerHTML += ` &#x1F3C1;`
+
+
+
+
+}
+
+
+}
